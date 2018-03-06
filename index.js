5a2ccc0392a389321e0bcd54728517575ee6de3d
Thummarong <thummarong2538@gmail.com>
2018-03-06 22:08:14 +0700

    tesr post

--- index.js
+++ index.js
@@ -49,3 +49,17 @@ app.get("/api/Customer", function(req, res) {
     }
   });
 });
+
+app.post("/api/Customer/newuser", function(req, res) {
+   var listname = req.body;
+
+  db.collection(CUSTOMER_COLLECTION).insert(listname,function(err, docs) {
+    res.send('Add name'+ docs.name)
+    if (err) {
+      handleError(res, err.message, "Failed to post contacts.");
+    } else {
+      res.status(201).json(docs);
+    }
+  });
+});
+
